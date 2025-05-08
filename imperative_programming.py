5dde810f882665d914e5fd0d5baafe77813b0c11
Alejandro JS <alejandro97js@gmail.com>
2025-05-08 19:29:08 +0200

    feat(imperative_programming): Example explained

--- imperative_programming.py
+++ imperative_programming.py
@@ -22,10 +22,29 @@ products = [
 # Gestión del inventario
 inventory = {product["sku"]: product for product in products}
 
+inventory = {}
+for product in products:
+    inventory[product["sku"]] = product
+
+# Ejemplo ejecucion:
+# Antes de iniciar:
+# {}
+# Tras primera vuelta:
+# {"SFU123": {"name": "Laptop", "sku": "...,}
+# Tras segunda vuelta:
+# {"SFU123": {"name": "Laptop", "sku": "...,
+# "SKU456": {"name": "Mouse", "sku": "SKU456", "price": 25,...
+# }
+# Tras tercera
+# {"SFU123": "Producto 1, con todos sus datos...",
+# "SKU456": "Producto 2..."
+# }
+
+
 # Procesar pedidos
 orders = [
-    {"order_id": "ORDER001", "items": {"SKU123": 2, "SKU456": 5}},
-    {"order_id": "ORDER002", "items": {"SKU789": 3, "SKU101": 1}},
+    {"order_id": "ORDER001", "items": {"SKU123": 7, "SKU456": 5}},
+    {"order_id": "ORDER002", "items": {"SKU123": 5, "SKU789": 3, "SKU101": 1}},
     {"order_id": "ORDER003", "items": {"SKU456": 10, "SKU101": 2}}
 ]
 
@@ -40,15 +59,12 @@ for order in orders:
             continue
         if product["current_stock"] < quantity:
             print(f"Error: Insufficient stock for {product['name']}. Available: {product['current_stock']}, Requested: {quantity}")
+            available_before_updating = product["current_stock"]
+            product["current_stock"] = 0 # Actualizar el stock quitando lo que se ha llevado en ese pedido.
+            total += product["price"] * product["current_stock"]
+            print(f"Pero le vendo {available_before_updating}")
             continue
         # Actualizar el stock
-        product["current_stock"] -= quantity
+        product["current_stock"] -= quantity # Actualizar el stock quitando lo que se ha llevado en ese pedido.
         total += product["price"] * quantity
     print(f"Order ID: {order_id} - Total: ${total:.2f} - Purchase Completed")
-
-# Mostrar estado final del inventario
-print("\nInventory Report:\n")
-for product in inventory.values():
-    category_names = ", ".join([cat["name"] for cat in product["categories"]]) or "None"
-    tag_names = ", ".join([tag["name"] for tag in product["tags"]]) or "None"
-    print(f"Product: {product['name']} (SKU: {product['sku']}) - Price: ${product['price']:.2f}, Stock: {product['current_stock']}, Categories: [{category_names}], Tags: [{tag_names}]")
